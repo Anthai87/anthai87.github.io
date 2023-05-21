951c019a21f87b79a3909af5d1a5fa2bdb34e2aa
anthai87 <Hashai87>
2023-05-21 13:55:09 +0200

    Navbar link test

--- src/_tests_/components/Navbar/Navbar.test.js
+++ src/_tests_/components/Navbar/Navbar.test.js
@@ -51,3 +51,39 @@ test("Test home ref and name", () => {
   expect(linkElement).toBeInTheDocument();
   expect(linkElement).toHaveAttribute("href", "/zee_web");
 });
+
+test("Test fashion ref and name", () => {
+  render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+
+  const linkElement = screen.getAllByText(/Fashion/i);
+  expect(linkElement[0]).toBeInTheDocument();
+  expect(linkElement[1]).toHaveAttribute("href", "/zee_web/products");
+});
+
+test("Test contact ref and name", () => {
+  render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+
+  const linkElement = screen.getAllByText(/Contact/i);
+  expect(linkElement[0]).toBeInTheDocument();
+  expect(linkElement[1]).toHaveAttribute("href", "/zee_web/contact");
+});
+
+test("Test about ref and name", () => {
+  render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+
+  const linkElement = screen.getAllByText(/About/i);
+  expect(linkElement[0]).toBeInTheDocument();
+  expect(linkElement[1]).toHaveAttribute("href", "/zee_web/about");
+});
